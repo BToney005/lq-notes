8e9d991cadade6ee09c1ddf57675a0a8ebc46ca9
Brandon <btoney434@gmail.com>
2021-04-11 12:58:13 -0400

    Added seeder for notes

---
+++ database/seeds/NoteSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Note;
+
+class NoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $notes = [
+            "This is a test note." => true,
+            "It was his hat Mr. Krabs, he was number one!" => false,
+            "Oh, these aren't pies, they're bombs. They were made in a bomb factory." => false,
+            "The best time to wear a striped sweater, is all the time." => false,
+            "I'd normally use faker for this." => true,
+            "I think I'll eat it now." => false,
+            "I need, I need, a tailor. Because I ripped my pants" => false,
+            "Gonna write an essay, that's what I say" => false,
+            "I should really watch SpongeBob right now" => true,
+            "Is mayonnaise an instrument?" => false
+        ];
+
+        foreach ($notes as $body => $delete_flag) {
+            $note = Note::create(['body' => $body]);
+            if ($delete_flag)
+                $note->delete();
+        }
+    }
+}
